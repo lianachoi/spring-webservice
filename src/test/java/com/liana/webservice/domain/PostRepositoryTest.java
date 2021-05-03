f95b52ad8ea04f1b5541e50f450ef6e92fd85f16
liana.choi <2ndsunmail@gmail.com>
2021-05-04 01:52:25 +0900

    도메인 코드 만들기

---
+++ src/test/java/com/liana/webservice/domain/PostRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.liana.webservice.domain;
+
+import com.liana.webservice.domain.posts.PostRepository;
+import com.liana.webservice.domain.posts.Posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @After
+    public void cleanup(){
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        postRepository.save(Posts.builder()
+        .title("테스트 게시글")
+        .content("테스트 본문")
+        .author("liana")
+        .build());
+
+        List<Posts> postsList = postRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(), is("테스트 게시글"));
+        assertThat(posts.getContent(), is("테스트 본문"));
+    }
+}
